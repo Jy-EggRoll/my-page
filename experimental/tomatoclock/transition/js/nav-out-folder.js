71f8ee6121e9dd259f494a2863b922f7e2b15741
JyEggRoll@outlook.com <JyEggRoll@outlook.com>
2025-01-03 23:05:22 +0800

    :memo: 同步了博客的地址

---
+++ experimental/tomatoclock/transition/js/nav-out-folder.js
@@ -0,0 +1,6 @@
+fetch('html/navigation-out-folder.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navigation-out-folder').innerHTML = data;
+    })
+    .catch(error => console.log('Error loading HTML:', error));
